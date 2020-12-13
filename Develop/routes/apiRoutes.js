a5976f7202c4c9b192c362264c338df4c7b3a9c1
axeliono <69375446+axeliono@users.noreply.github.com>
2020-12-13 17:00:58 -0600

    finished basic functions, working on editing existing note

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,48 +3,27 @@ const data = require("../db/db.json");
 
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf-8", (err, notes) => {
-      if (err) throw err;
-      let parsed;
-      try {
-        parsed = [].concat(JSON.parse(notes));
-      } catch (err) {
-        parsed = [];
-      }
-
-      res.json(parsed);
-    });
+    res.json(data);
   });
 
   app.post("/api/notes", (req, res) => {
-    console.log(data, "new note being added", req.body);
-    fs.readFile("./db/db.json", "utf-8", (err, resp) => {
-      let allNotes = JSON.parse(resp);
-
-      var recentNote = allNotes[allNotes.length - 1].id;
-      recentNote = recentNote + 1;
-
-      const createdNote = { ...req.body, id: recentNote };
-      allNotes.push(createdNote);
-      fs.writeFile("./db/db.json", JSON.stringify(allNotes), (err) => {
-        if (err) throw err;
-        res.json(allNotes);
-      });
-    });
+    newNote = req.body;
+    newNote.id = data.length.toString();
+    data.push(newNote);
+    res.json(newNote);
   });
 
   app.delete("/api/notes/:id", (req, result) => {
     let noteId = req.params.id;
-
-    fs.readFile("./db/db.json", "utf-8", (err, response) => {
-      if (err) throw err;
-      const allNotes = JSON.parse(response);
-      const newNotes = allNotes.filter((note) => note.id != noteId);
-      fs.writeFile("./db/db.json", JSON.stringify(newNotes, null, 2), (err) => {
-        if (err) throw err;
-        result.json(true);
-        console.log("Notes have been deleted!");
-      });
-    });
+    for (i = 0; i < data.length; i++) {
+      if (data[i].id === noteId) {
+        data.splice(i, 1);
+        break;
+      }
+    }
+    for (i = 0; i < data.length; i++) {
+      data[i].id = i.toString();
+    }
+    return res.json(true);
   });
 };
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,8 +1,16 @@
 const path = require("path");
 
 module.exports = (app) => {
+  app.use("*.js", (req, res, next) => {
+    res.set("Content-Type", "text/javascript");
+    next();
+  });
   app.get("/notes/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  app.get("/assets/js/index.js", (req, res) => {
+    res.sendfile(path.join(__dirname, "../public/assets/js/index.js"));
   });
 
   app.get("*", (req, res) => {
